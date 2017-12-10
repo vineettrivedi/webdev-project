e7a3a99747a80727185a615b345ca71c31a6301c
Vineet Trivedi <trivedi.v@husky.neu.edu>
2017-12-10 14:37:43 -0500

    test db

--- dist/main.bundle.js
+++ dist/main.bundle.js
@@ -2203,7 +2203,7 @@ module.exports = module.exports.toString();
 /***/ "../../../../../src/app/components/home/home.component.html":
 /***/ (function(module, exports) {
 
-module.exports = "\n<div class=\"container\">\n\n  <h1>\n    Angular 4 MEAN stack app\n  </h1>\n\n\n  <h3>\n    App works\n  </h3>\n\n\n  <h4>\n    <a href=\"test\">Test MongoDB</a>\n  </h4>\n\n  <h3>\n    <a [routerLink]=\"['home']\">Home</a>\n  </h3>\n\n</div>\n\n"
+module.exports = "\n<div class=\"container\">\n\n  <h1>\n    Angular 4 MEAN stack app\n  </h1>\n\n\n  <h3>\n    App works\n  </h3>\n\n\n  <h4>\n    <a [routerLink]=\"['test']\">Test MongoDB</a>\n  </h4>\n\n  <h3>\n    <a [routerLink]=\"['home']\">Home</a>\n  </h3>\n\n</div>\n\n"
 
 /***/ }),
 
